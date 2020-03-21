2bcaa2d1cb701f464c714740175c695a70adc320
Debarshi <debarkhisonowal@gmail.com>
2020-03-21 11:13:44 +0530

    assist

--- app/src/main/java/com/deb/demoapp/MessageFragment.java
+++ app/src/main/java/com/deb/demoapp/MessageFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Spinner;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,36 +16,48 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 public class MessageFragment extends Fragment {
 
     //Variables
-    ListView mListView;
+    Spinner mSpinner;
     EditText mEditText;
     Button mButton;
-    DatabaseReference root;
+    DatabaseReference root,local ;
     String ab;
+    private ArrayAdapter<String> adapter;
+    private List<String> arrayList;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false );
+       View rootView = inflater.inflate(R.layout.fragment_message, container, false );
+        mSpinner = rootView.findViewById(R.id.spinner);
+        mButton = rootView.findViewById(R.id.bt_save);
+        mEditText = rootView.findViewById(R.id.ed_msg);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messagelist messagelist = new messagelist(mEditText.getText().toString());
+                root.child("Messages").push().setValue(messagelist);
+            }
+        });
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.messages));
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mAdapter);
+        return rootView;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    mButton = mButton.findViewById(R.id.bt_save);
-    mEditText = mEditText.findViewById(R.id.ed_msg);
-    mListView = mListView.findViewById(R.id.ls_msg);
+        root = FirebaseDatabase.getInstance().getReference();
 
-    root = FirebaseDatabase.getInstance().getReference();
 
-    mButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            ab =
-        }
-    });
+
     }
+
 }
---
+++ app/src/main/java/com/deb/demoapp/messagelist.java
@@ -0,0 +1,17 @@
+package com.deb.demoapp;
+
+public class messagelist {
+    private String text;
+
+    public messagelist(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
